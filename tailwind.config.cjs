785ece7ae33a3906a8ded8058b64e24f2351b519
Alvi Hysa <alvihysa89@gmail.com>
2023-09-19 14:16:10 +0200

    Ready. Set. Go!

---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    // courier new monospace font
+    extend: {
+      fontFamily: {
+        serif: ["Trebuchet MS", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
